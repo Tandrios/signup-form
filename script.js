a7259a86ccba84a97c2c60b266b782144df055dc
Tandrios <t.andriessen@gmail.com>
2023-11-17 21:24:07 +0100

    Version 1.0

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
+    inputPass = document.querySelector('#password')
+    inputConfirm = document.querySelector('#confirmation')
 
-    
+    inputPass.addEventListener('keyup', (e) => {
+        checkPass(inputPass, inputConfirm)
+    })
+    inputConfirm.addEventListener('keyup', (e) => {
+        checkPass(inputPass, inputConfirm)
+    })
 })
+
+function checkPass(inputPass, inputConfirm) {
+    if (inputPass.value !== inputConfirm.value) {
+        document.querySelector('#errormsg').textContent = "* Passwords do not match"
+    } else {
+        document.querySelector('#errormsg').textContent = ""
+    }
+}
